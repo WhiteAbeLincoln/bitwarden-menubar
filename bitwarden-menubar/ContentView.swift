c69a13aae416308e49b4d9c9b8802fe2e73d8438
Abraham White <abelincoln.white@gmail.com>
2021-03-10 18:03:59 -0700

    Initial Commit

---
+++ bitwarden-menubar/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  bitwarden-menubar
+//
+//  Created by Abraham White on 3/10/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
